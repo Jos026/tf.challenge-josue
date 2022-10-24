37fb0634683f3bec93e7c01660448c71ff7c08a1
Jos026 <josue.ocampo.soto@gmail.com>
2022-10-23 20:19:06 -0600

    new changes

--- aws_bucket/ec2-aws.tf
+++ aws_bucket/ec2-aws.tf
@@ -1,11 +1,10 @@
 resource "aws_instance" "clase06" {
     ami = var.ec2-ami
     instance_type = var.ec2-instance-type
-    key_name = "demo-clase06"
 
 
     tags = {
-      "Name" = "MiEC2DemoClase06"
+      "Name" = "EC2_Challenge"
     }
   
 }
